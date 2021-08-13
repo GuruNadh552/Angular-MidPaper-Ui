f057b324ecf6cd1034b05c737491de87979ebcc5
Guru1605 <gurunadhpukkalla@gmail.com>
2021-08-13 11:48:15 +0530

    V3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { QuestionComponent } from './question/question.component';
 import { QinputComponent } from './qinput/qinput.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionComponent,
     QinputComponent,
   ],
   imports: [
--- src/app/qinput/qinput.component.ts
+++ src/app/qinput/qinput.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QinputComponent implements OnInit {
 
-
+  i=1;
   ques = ["q1","q2","q3","q4","q5","q6","q7","q8","q9","q10"]
   ans = ["","","","","","","","","",""]
 
@@ -16,6 +16,18 @@ export class QinputComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  
+  printPaper(){
+    window.print();
+  }
+
+  submitVal(){
+    if(this.ans.includes("")){
+      this.i = 1;
+      alert("Please Enter All Questions");
+    }
+    else{
+      this.i=0;
+    }
+  }
 
 }
